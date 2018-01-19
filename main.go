609f6ea04f2cbc342d4a0b74b4036938862c3995
Ravern Koh <ravern.koh.dev@gmail.com>
2018-01-19 09:14:34 +0000

    Launches dmenu properly

--- main.go
+++ main.go
@@ -1,37 +1,102 @@
 package main
 
 import (
+	"bytes"
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"os"
+	"os/exec"
 	"strings"
+
+	tilde "gopkg.in/mattes/go-expand-tilde.v1"
 )
 
-const (
-	cli = "cli"
-	gui = "gui"
-)
+var files = map[string][]string{
+	"cli": []string{"i3-sensible-terminal", "-e"},
+	"gui": []string{},
+}
 
 type config struct {
 	path string
-	args string
+	args []string
 }
 
 func main() {
-	cfg := new(config)
+	cfg := config{}
 
-	// Extract the -- flag first
-	for i, arg := range os.Args {
-		if arg == "--" {
-			cfg.args = strings.Join(os.Args[i+1:], " ")
-			os.Args = os.Args[:i]
-			break
+	// Parse the configuration
+	cfg.args = extractArgs()
+	err := parseFlags(&cfg)
+	check(err)
+
+	var (
+		cmdsStr = &bytes.Buffer{}
+		cmdsIdx = map[string]string{}
+	)
+
+	// Read content files and add to command list
+	for f := range files {
+		s, err := ioutil.ReadFile(cfg.path + "/" + f)
+		check(err)
+		ls := strings.Split(string(s), "\n")
+
+		// Add each one with type of file
+		for _, l := range ls {
+			if l != "" {
+				cmdsStr.WriteString(l + "\n")
+				cmdsIdx[l] = f
+			}
 		}
 	}
 
-	// Parse the rest of the flags
+	// Run the dmenu command
+	dmenu := exec.Command("dmenu", cfg.args...)
+	dmenu.Env = os.Environ()
+	dmenu.Stdin = cmdsStr
+	app, err := dmenu.Output()
+	check(err)
+
+	// Execute the chosen app
+	fmt.Println(string(app))
+}
+
+// extractArgs returns a string of everything after a -- flag and
+// removes them from os.Args.
+func extractArgs() []string {
+	for i, arg := range os.Args {
+		if arg == "--" {
+			defer func() {
+				os.Args = os.Args[:i]
+			}()
+			return os.Args[i+1:]
+		}
+	}
+	return nil
+}
+
+// parseFlags parses os.Args and returns a config object, along
+// with a possible error.
+func parseFlags(cfg *config) error {
+	var tmp string // for help text, is a no-op
+	flag.StringVar(&tmp, "-", "", "args to pass to dmenu")
 	flag.StringVar(&cfg.path, "path", "~/.config/dmenual", "path to config dir")
 	flag.Parse()
 
-	fmt.Println(cfg)
+	// Expand the possible tilde in the path
+	path, err := tilde.Expand(cfg.path)
+	if err != nil {
+		return err
+	}
+	cfg.path = path
+
+	return nil
+}
+
+// check prints the error and exists if the error is non-nil.
+func check(err error) {
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
